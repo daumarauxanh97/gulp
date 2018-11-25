76457f6e3fc6f96ae7e6f008c4a32bbe57cd06dc
daumarauxanh97 <37074701+daumarauxanh97@users.noreply.github.com>
2018-11-25 23:23:01 +0700

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -32,3 +32,12 @@ gulp.task('browserSync', function() {
         },
     })
 })
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+gulp.task('images', function(){
+    return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
+        .pipe(cache(imagemin({
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+});
